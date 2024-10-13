749bc5efa6b1751455b9c49625edee00724749e2
marinaacastroo <129770053+marinaacastroo@users.noreply.github.com>
2024-10-13 12:37:14 +0200

    Create system.py

---
+++ system.py
@@ -0,0 +1,5 @@
+from surprise import Dataset, Reader, KNNBasic
+from surprise.model_selection import train_test_split
+
+# Cargar el conjunto de datos
+file_path = 'C:/Users/marin/OneDrive/Documentos/IA2024-2025/RAIN/dataset.csv'
